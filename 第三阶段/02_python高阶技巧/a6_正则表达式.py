9c591f1cd2049a050256f2f1d88a52d2cd0f1d71
HammerRay <zhao131804@gmail.com>
2024-03-03 00:14:25 +0800

    #1[基础知识][基础语法][2024.2.29] 正则表达式

--- 第三阶段/02_python高阶技巧/a6_正则表达式.py
+++ 第三阶段/02_python高阶技巧/a6_正则表达式.py
@@ -6,7 +6,30 @@ print(result)
 print(result.span())
 print(result.group())
 
-s = '1python itheima python itheima python itheima python'
-
+s = '1python2 3itheima4 5python7 itheima 1000p***ython ###itheima python'
+# 从头匹配
 result = re.match('python', s)
 print(result)
+# 搜索匹配
+result = re.search('python', s)
+print(result)
+# findall 搜索全部匹配
+result = re.findall('python', s)
+print(result)
+
+# 元字符匹配
+# 我一直认为从事脑力劳动，这种思考的活动，完全可以做到体力劳动者做不到的，劳动14个小时甚至更久，并且是越用越强。 从此观点上看，自己美好的生活令我十分的
+# 宽慰畅心，并且可以开心的同时创造更多的价值产出，充实我的人生，追逐我的梦想。
+# 找出第一个数字
+result = re.search(r"\d", s)
+print(result)
+# 找出特殊字符
+result = re.findall(r"\W", s)
+print(result)
+# 找出全部英文字母
+result = re.findall(r"[a-zA-Z]", s)
+print(result)
+
+# 案例练习
+# 邮箱
+result = re.match(r"^[\w-]+(\.[\w-]+)*@(qq|com|gmail)(\.[\w-]+)+$")
