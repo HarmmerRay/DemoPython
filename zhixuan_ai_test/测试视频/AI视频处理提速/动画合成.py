fc65b6a5c5439d9ac2df8e8f373cfaa562218487
HammerRay <zhao131804@gmail.com>
2024-10-30 11:22:20 +0800

    1

--- zhixuan_ai_test/测试视频/AI视频处理提速/动画合成.py
+++ zhixuan_ai_test/测试视频/AI视频处理提速/动画合成.py
@@ -24,18 +24,23 @@ start_time = datetime.datetime.now()
 # duration = float(pipe.read().strip())
 # print(duration)
 # 全屏
-# ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1[ov];[1:v][ov]overlay=x=W-w:y=H-h:eof_action=repeat" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
+ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1[ov];[1:v][ov]overlay=x=W-w:y=H-h:eof_action=repeat:enable='lte(t,30)'" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
 # 顶部
-# ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1[ov];[1:v][ov]overlay=x=0:y=H*-0.8:eof_action=repeat" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
+# ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1[ov];[1:v][ov]overlay=x=0:y=H*-0.8:eof_action=repeat:enable='lte(t,60)'" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
+# 底部
+# ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1[ov];[1:v][ov]overlay=x=0:y=H*0.8:eof_action=repeat" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
 # 上下方
 # ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -i xiayu.mov -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1[ov];[1:v][ov]overlay=x=0:y=H*-0.8:eof_action=repeat[av];[av][2:v]overlay=x=0:y=H*0.8:eof_action=repeat" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
 # 左上角
 # ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1,scale=iw*1.5:ih*1.5,crop=1080*0.2:1920*0.2:1080*0.2:1920*0.2[ov];[1:v][ov]overlay=x=0:y=0:eof_action=repeat" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
 # 右上角
-ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1,scale=iw*1.5:ih*1.5,crop=1080*0.2:1920*0.2:1080*0.2:1920*0.2[ov];[1:v][ov]overlay=x=W-w:y=0:eof_action=repeat" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
+# ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1,scale=iw*1.5:ih*1.5,crop=1080*0.2:1920*0.2:iw/4:ih/2[ov];[1:v][ov]overlay=x=W-w:y=0:eof_action=repeat" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
 # 左下角
+# ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1,scale=iw*1.5:ih*1.5,crop=1080*0.2:1920*0.2:iw/4:ih/2[ov];[1:v][ov]overlay=x=0:y=H-h:eof_action=repeat" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
 # 右下角
-# 底部
+# ffmpeg_cmd = f"""ffmpeg -stream_loop -1 -i xiayu.mov -i input.mp4 -filter_complex "[0:v]format=argb,fade=in:st=0:d=1:alpha=1,scale=iw*1.5:ih*1.5,crop=1080*0.2:1920*0.2:iw/4:ih/2[ov];[1:v][ov]overlay=x=W-w:y=H-h:eof_action=repeat" -map 1:a -c:a copy -c:v rawvideo -pix_fmt yuv420p -preset ultrafast -t {duration} -y output.avi"""
+
+
 os.system(ffmpeg_cmd)
 #
 end_time = datetime.datetime.now()
